acc37fd4861306243f2b99c0039c82c8c5744a84
Barath Kumar <kbarath3694@gmail.com>
2024-02-15 14:54:40 +0530

    Initial Commit
    My new portfolio with Bootstrap 5

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// Navbar JS
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+        $('.fixed-top').css('background', 'transparent');
+    } else{
+        $('.fixed-top').css('background', 'rgba(0, 0, 0, 0.6)');
+    }
+});
