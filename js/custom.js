639fac9d665cc08b13b893ccc59c8c43b5cf0e09
Barath <barath.k@oasyscybernetics.com>
2024-10-05 21:35:56 +0530

    Final_Commit-v1
    Added Resume pdf, Contact Form Fix, Navbar active fix

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,16 @@ $(window).scroll(function(){
         $('.fixed-top').css('background', 'rgba(0, 0, 0, 0.6)');
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-item");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            // Remove active class from all links
+            navLinks.forEach(item => item.classList.remove("active"));
+            // Add active class to the clicked link
+            this.classList.add("active");
+        });
+    });
+});
